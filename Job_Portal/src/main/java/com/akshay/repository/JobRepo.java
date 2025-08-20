38db4b591d521452feea9b4c51055fa303b514c0
Akshay Kadam <akshaykadam2321@gmail.com>
2025-08-20 13:15:10 +0530

    Add files via upload

---
+++ Job_Portal/src/main/java/com/akshay/repository/JobRepo.java
@@ -0,0 +1,18 @@
+package com.akshay.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.akshay.entity.JobPost;
+
+
+
+@Repository
+public interface JobRepo extends JpaRepository<JobPost, Integer> {
+
+	List<JobPost> findByPostProfileContainingOrPostDescContaining(String postProfile, String postDesc);
+
+
+}
